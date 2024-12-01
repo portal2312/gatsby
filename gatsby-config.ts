d49379175aaa4dc04b3d0085178148d24b4edf11
portal2312 <portal2312@gmail.com>
2024-12-02 00:10:27 +0900

    Finished Gatsby Tutorial Part 7

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -44,7 +44,7 @@ const config: GatsbyConfig = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/src/pages/blog`,
       },
       __key: "blog",
     },
--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
@@ -11,9 +12,26 @@ interface BlogPostProps {
 }
 
 const BlogPost = ({ data, children }: BlogPostProps) => {
+  const image = getImage(
+    data.mdx?.frontmatter?.hero_image?.childImageSharp?.gatsbyImageData ?? null,
+  );
   return (
     <Layout pageTitle={data.mdx?.frontmatter?.title}>
       <p>{data.mdx?.frontmatter?.date}</p>
+      {image && data.mdx?.frontmatter?.hero_image_alt && (
+        <GatsbyImage
+          image={image}
+          alt={data.mdx?.frontmatter?.hero_image_alt}
+        />
+      )}
+      <p>
+        Photo Credit:{" "}
+        {data.mdx?.frontmatter?.hero_image_credit_link && (
+          <a href={data.mdx?.frontmatter?.hero_image_credit_link}>
+            {data.mdx?.frontmatter?.hero_image_credit_text}
+          </a>
+        )}
+      </p>
       {children}
     </Layout>
   );
@@ -24,7 +42,15 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
